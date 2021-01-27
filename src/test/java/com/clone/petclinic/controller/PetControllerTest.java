ac5a4e174efcad78092957dd7c434355511577da
kobeomseok95 <37062337+kobeomseok95@users.noreply.github.com>
2021-01-27 22:44:18 +0900

    Refactor : PetController

--- src/test/java/com/clone/petclinic/controller/PetControllerTest.java
+++ src/test/java/com/clone/petclinic/controller/PetControllerTest.java
@@ -3,8 +3,14 @@ package com.clone.petclinic.controller;
 import com.clone.petclinic.controller.dto.OwnerOneResponseDto;
 import com.clone.petclinic.controller.dto.OwnerPetsResponseDto;
 import com.clone.petclinic.controller.dto.PetJoinAndEditDto;
+import com.clone.petclinic.domain.Owner;
+import com.clone.petclinic.domain.Pet;
+import com.clone.petclinic.domain.PetType;
+import com.clone.petclinic.domain.base.Address;
+import com.clone.petclinic.service.OwnerService;
 import com.clone.petclinic.service.PetService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -16,10 +22,12 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.HashSet;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -36,112 +44,153 @@ class PetControllerTest {
     ObjectMapper objectMapper;
 
     @MockBean
+    OwnerService ownerService;
+
+    @MockBean
     PetService petService;
+
+    Owner owner;
+    Pet pet;
+    PetType type;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        type = PetType.builder()
+                .id(1L)
+                .name("snake")
+                .build();
+
+        owner = Owner.builder()
+                .id(3L)
+                .firstName("owner1")
+                .lastName("owner1")
+                .phone("1234")
+                .pets(new HashSet<>())
+                .address(
+                        Address.builder()
+                                .city("test")
+                                .street("test")
+                                .zipcode("test")
+                                .build()
+                )
+                .build();
+
+        pet = Pet.builder()
+                .id(2L)
+                .petType(type)
+                .date(LocalDate.now())
+                .name("pet1")
+                .visits(new HashSet<>())
+                .build();
+        pet.addOwner(owner);
+    }
 
     @Test
     void pet_등록() throws Exception{
 
         //given
-        PetJoinAndEditDto request = createPetJoinDto();
-        OwnerOneResponseDto response = createOneResponseDto(request);
-//        when(petService.addPet(any(Long.class), any(PetJoinAndEditDto.class)))
-//                .thenReturn(response);
+        PetJoinAndEditDto requestDto = createPetJoinRequestDto(owner);
+        OwnerOneResponseDto responseDto = createPetJoinResponseDto(requestDto);
+        when(ownerService.findOne(any(Long.class)))
+                .thenReturn(responseDto);
 
         //when, then
-        mockMvc.perform(post("/owners/{id}/pets/new", response.getId())
-                        .content(objectMapper.writeValueAsString(request))
+        mockMvc.perform(post("/owners/{ownerId}/pets/new", owner.getId())
+                        .content(objectMapper.writeValueAsString(requestDto))
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.id", is("3")))
-                .andExpect(jsonPath("$.name", is("owner3")))
-                .andExpect(jsonPath("$.phone", is("test")))
+                .andExpect(jsonPath("$.name", is("owner1 owner1")))
+                .andExpect(jsonPath("$.phone", is("1234")))
                 .andExpect(jsonPath("$.city", is("test")))
                 .andExpect(jsonPath("$.street", is("test")))
                 .andExpect(jsonPath("$.zipcode", is("test")))
-                .andExpect(jsonPath("$.pets[0].id", is("1")))
-                .andExpect(jsonPath("$.pets[0].name", is("pet1")))
-                .andExpect(jsonPath("$.pets[0].birth").exists())
-                .andExpect(jsonPath("$.pets[0].type", is("snake")));
+                .andExpect(jsonPath("$.pets.length()", is(1)));
+        verify(ownerService).findOne(any(Long.class));
     }
 
     @Test
     void pet_수정() throws Exception{
 
         //given
-        PetJoinAndEditDto requestDto = createPetEditRequestDto();
+        PetJoinAndEditDto requestDto = createPetEditRequestDto(owner, pet);
         OwnerOneResponseDto responseDto = createOwnerOneResponseDtoForPetEdit(requestDto);
-//        when(petService.editPet(any(Long.class), any(Long.class), any(PetJoinAndEditDto.class)))
-//                .thenReturn(responseDto);
+        when(ownerService.findOne(any(Long.class)))
+                .thenReturn(responseDto);
 
         //when, then
-        mockMvc.perform(put("/owners/{ownerId}/pets/{petId}/edit", 1L, 1L)
+        mockMvc.perform(put("/owners/{ownerId}/pets/{petId}/edit",
+                                owner.getId(), pet.getId())
                         .content(objectMapper.writeValueAsString(requestDto))
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.id", is("1")))
-                .andExpect(jsonPath("$.name", is("owner1")))
+                .andExpect(jsonPath("$.id", is("3")))
+                .andExpect(jsonPath("$.name", is("owner1 owner1")))
                 .andExpect(jsonPath("$.pets.length()", is(1)))
-                .andExpect(jsonPath("$.pets[0].id", is("1")))
+                .andExpect(jsonPath("$.pets[0].id", is("2")))
                 .andExpect(jsonPath("$.pets[0].birth").exists())
                 .andExpect(jsonPath("$.pets[0].name", is("edit")))
                 .andExpect(jsonPath("$.pets[0].type", is("bird")))
                 .andDo(print());
     }
 
-    private OwnerOneResponseDto createOwnerOneResponseDtoForPetEdit(PetJoinAndEditDto request) {
+    private OwnerOneResponseDto createOwnerOneResponseDtoForPetEdit(PetJoinAndEditDto requestDto) {
         return OwnerOneResponseDto.builder()
-                .id(request.getOwnerId())
-                .name(request.getOwnerName())
+                .id(requestDto.getOwnerId())
+                .name(requestDto.getOwnerName())
                 .pets(
                         Arrays.asList(
                                 OwnerPetsResponseDto.builder()
-                                        .id(request.getPetId())
-                                        .birth(request.getPetBirth())
-                                        .name(request.getPetName())
-                                        .type(request.getPetType())
+                                        .id(requestDto.getPetId())
+                                        .birth(requestDto.getPetBirth())
+                                        .name(requestDto.getPetName())
+                                        .type(requestDto.getPetType())
                                         .build()
                         )
                 )
                 .build();
     }
 
-    private PetJoinAndEditDto createPetEditRequestDto() {
+    private PetJoinAndEditDto createPetEditRequestDto(Owner owner, Pet pet) {
         return PetJoinAndEditDto.builder()
-                .ownerId("1")
-                .ownerName("owner1")
-                .petId("1")
+                .ownerId(owner.getId().toString())
+                .ownerName(owner.getFirstName() + " " + owner.getLastName())
+                .petId(pet.getId().toString())
                 .petName("edit")
                 .petType("bird")
                 .petBirth(LocalDate.now().toString())
                 .build();
     }
 
-    private PetJoinAndEditDto createPetJoinDto() {
+    private PetJoinAndEditDto createPetJoinRequestDto(Owner owner) {
         return PetJoinAndEditDto.builder()
+                .ownerId(owner.getId().toString())
+                .ownerName(owner.getFirstName() + " " + owner.getLastName())
+                .petType("bird")
+                .petName("add")
                 .petBirth(LocalDate.now().toString())
-                .petName("pet1")
-                .petType("snake")
                 .build();
     }
 
-    private OwnerOneResponseDto createOneResponseDto(PetJoinAndEditDto request) {
+    private OwnerOneResponseDto createPetJoinResponseDto(PetJoinAndEditDto requestDto) {
         return OwnerOneResponseDto.builder()
-                .id("3")
-                .name("owner3")
-                .phone("test")
-                .city("test")
-                .street("test")
-                .zipcode("test")
+                .id(owner.getId().toString())
+                .name(owner.getFirstName() + " " + owner.getLastName())
+                .city(owner.getAddress().getCity())
+                .street(owner.getAddress().getStreet())
+                .zipcode(owner.getAddress().getZipcode())
+                .phone(owner.getPhone())
                 .pets(Arrays.asList(
                         OwnerPetsResponseDto.builder()
-                                .id("1")
-                                .type(request.getPetType())
-                                .name(request.getPetName())
-                                .birth(request.getPetBirth())
-                                .build()))
+                                .id("3")
+                                .name(requestDto.getPetName())
+                                .type(requestDto.getPetType())
+                                .birth(requestDto.getPetBirth())
+                                .build()
+                ))
                 .build();
     }
 }
